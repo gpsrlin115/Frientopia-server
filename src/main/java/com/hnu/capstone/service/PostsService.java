607421935c9498d06442653e47e2cac9210a2b36
Jae-Young98 <ygy0102@gmail.com>
2023-05-05 00:33:18 +0900

    3장 jpa

---
+++ src/main/java/com/hnu/capstone/service/PostsService.java
@@ -0,0 +1,37 @@
+package com.hnu.capstone.service;
+
+import com.hnu.capstone.domain.posts.Posts;
+import com.hnu.capstone.domain.posts.PostsRepository;
+import com.hnu.capstone.dto.PostsResponseDto;
+import com.hnu.capstone.dto.PostsSaveRequestDto;
+import com.hnu.capstone.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
