46819a1badf573f8e10c60fd9b3f17f76ee73d02
Jae-Young98 <ygy0102@gmail.com>
2023-05-21 22:22:35 +0900

    Revert "merge with posts"
    This reverts commit ff34f0a16622b8501f7a8c3f7b41c268d52ac7d6.

--- src/main/java/com/hnu/capstone/controller/IndexController.java
+++ src/main/java/com/hnu/capstone/controller/IndexController.java
@@ -34,7 +34,6 @@ public class IndexController {
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
-            model.addAttribute("userRole", userService.SelectUser(user.getEmail()).getRole().name());
         }
         return "index";
     }
@@ -45,7 +44,6 @@ public class IndexController {
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
-            model.addAttribute("userRole", userService.SelectUser(user.getEmail()).getRole().name());
         }
         List<PostsListResponseDto> posts = postsService.findAllDesc();
         model.addAttribute("posts", posts);
@@ -59,12 +57,6 @@ public class IndexController {
         if(user != null) {
             model.addAttribute("userName", user.getName());
             model.addAttribute("userEmail", user.getEmail());
-            model.addAttribute("userRole", userService.SelectUser(user.getEmail()).getRole().name());
-            if(userService.SelectUser(user.getEmail()).getId() == 1){
-                User admin = userService.SelectUser(user.getEmail());
-                admin.roleUpdate(Role.ADMIN);
-                userService.UpdateUser(admin);
-            }
             if (userService.SelectUser(user.getEmail()).getRole() == Role.GUEST){
                 return "signUp";
             }
@@ -87,12 +79,10 @@ public class IndexController {
             user.roleUpdate(Role.USER);
 
             userService.UpdateUser(user);
-
         }
 
         return "redirect:/";
     }
-
 
     @GetMapping("/myPage")
     public String myPageForm(Model model){
@@ -106,7 +96,6 @@ public class IndexController {
         model.addAttribute("userMajor", user.getMajor());
         model.addAttribute("userPhoneNum", user.getPhoneNum());
         model.addAttribute("userIntroduce", user.getIntroduce());
-        model.addAttribute("userRole", user.getRole().name());
         return "myPage";
     }
 
@@ -137,22 +126,6 @@ public class IndexController {
         return "redirect:/";
     }
 
-<<<<<<< HEAD
-=======
-    @GetMapping("/admin")
-    public String adminForm(Model model) {
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user != null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-            model.addAttribute("userRole", userService.SelectUser(user.getEmail()).getRole().name());
-        }
-        List<User> users = userService.SelectAll();
-        model.addAttribute("users", users);
-        return "admin";
-    }
-
->>>>>>> master
     @GetMapping("/posts/save")
     public String postsSave(Model model)
     {
