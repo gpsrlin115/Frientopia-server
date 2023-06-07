bef26574dd0348d45625ec02195692acf83994ae
ShimSeohyun <20200580@gm.hannam.ac.kr>
2023-06-08 04:14:11 +0900

    google map api를 추가하여 멘토링을 진행할 위치를 찍을 수 있고, 그를 바탕으로 거리를 기준으로 filtering 하는 기능 구현, user phonenum input 형식 지정, 신청 기록 존재 시 신청버튼 비활성화
    Signed-off-by: ShimSeohyun <20200580@gm.hannam.ac.kr>

--- src/main/java/com/hnu/capstone/dto/PostsSaveRequestDto.java
+++ src/main/java/com/hnu/capstone/dto/PostsSaveRequestDto.java
@@ -5,6 +5,8 @@ import com.hnu.capstone.domain.User;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.Column;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -16,13 +18,19 @@ public class PostsSaveRequestDto {
     private User user;
     private String fileName;
     private MultipartFile file;
+    private double latitude;  // 위도
+    private double longitude; // 경도
+    private String address;   // 멘토링 주소
 
     @Builder
-    public PostsSaveRequestDto(String title, String content, String author, User user) {
+    public PostsSaveRequestDto(String title, String content, String author, User user, double latitude, double longitude, String address) {
         this.title = title;
         this.content = content;
         this.author = author;
         this.user = user;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
     }
 
     public PostsSaveRequestDto(String author) {
@@ -37,6 +45,9 @@ public class PostsSaveRequestDto {
                 .author(author)
                 .user(user)
                 .fileName(fileName)
+                .latitude(latitude)
+                .longitude(longitude)
+                .address(address)
                 .build();
     }
     /* 서버가 관리하는 파일명 추가 */
