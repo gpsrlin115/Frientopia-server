69b74918379a5d176df5f49428126ddfa8a32114
Jae-Young98 <86467141+Jae-Young98@users.noreply.github.com>
2023-06-11 02:32:30 +0900

    REVERT

--- src/main/java/com/hnu/capstone/config/WebConfig.java
+++ src/main/java/com/hnu/capstone/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.hnu.capstone.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,11 +10,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/post_upload/**")
                 .addResourceLocations("file:C:\\Users\\ygy01\\IdeaProjects\\capstone\\src\\main/resources/static/post_upload/");
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://ec2-15-164-141-152.ap-northeast-2.compute.amazonaws.com:8080");
     }
 }
